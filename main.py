6979bb15c1f5289af01c993834432af684a1f944
lailvlong <594379509@qq.com>
2019-12-23 10:57:33 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,173 @@
+import collections
+import os
+import random
+import tarfile
+import torch
+from torch import nn
+import torchtext.vocab as Vocab
+import torch.utils.data as Data
+from tqdm import tqdm
+import time
+
+import sys
+sys.path.append("..")
+
+DATA_ROOT = "\\S1\\CSCL\\tangss\\Datasets"
+
+def read_imdb(folder='train', data_root="S1\\CSCL\\tangss\\Datasets\\aclImdb"):  # 本函数已保存在d2lzh_pytorch包中方便以后使用
+    data = []
+    for label in ['pos', 'neg']:
+        folder_name = os.path.join(data_root, folder, label)
+        for file in tqdm(os.listdir(folder_name)):
+            with open(os.path.join(folder_name, file), 'rb') as f:
+                review = f.read().decode('utf-8').replace('\n', '').lower()
+                data.append([review, 1 if label == 'pos' else 0])
+    random.shuffle(data)
+    return data
+
+def get_tokenized_imdb(data):  # 本函数已保存在d2lzh_pytorch包中方便以后使用
+    """
+    data: list of [string, label]
+    """
+    def tokenizer(text):
+        return [tok.lower() for tok in text.split(' ')]
+    return [tokenizer(review) for review, _ in data]
+
+def get_vocab_imdb(data):  # 本函数已保存在d2lzh_pytorch包中方便以后使用
+    tokenized_data = get_tokenized_imdb(data)
+    counter = collections.Counter([tk for st in tokenized_data for tk in st])
+    return Vocab.Vocab(counter, min_freq=5)
+
+def preprocess_imdb(data, vocab):  # 本函数已保存在d2lzh_torch包中方便以后使用
+    max_l = 500  # 将每条评论通过截断或者补0，使得长度变成500
+
+    def pad(x):
+        return x[:max_l] if len(x) > max_l else x + [0] * (max_l - len(x))
+
+    tokenized_data = get_tokenized_imdb(data)
+    features = torch.tensor([pad([vocab.stoi[word] for word in words]) for words in tokenized_data])
+    labels = torch.tensor([score for _, score in data])
+    return features, labels
+
+
+class BiRNN(nn.Module):
+    def __init__(self, vocab, embed_size, num_hiddens, num_layers):
+        super(BiRNN, self).__init__()
+        self.embedding = nn.Embedding(len(vocab), embed_size)
+
+        # bidirectional设为True即得到双向循环神经网络
+        self.encoder = nn.LSTM(input_size=embed_size,
+                               hidden_size=num_hiddens,
+                               num_layers=num_layers,
+                               bidirectional=True)
+        self.decoder = nn.Linear(4 * num_hiddens, 2)  # 初始时间步和最终时间步的隐藏状态作为全连接层输入
+
+    def forward(self, inputs):
+        # inputs的形状是(批量大小, 词数)，因为LSTM需要将序列长度(seq_len)作为第一维，所以将输入转置后
+        # 再提取词特征，输出形状为(词数, 批量大小, 词向量维度)
+        embeddings = self.embedding(inputs.permute(1, 0))
+        # rnn.LSTM只传入输入embeddings，因此只返回最后一层的隐藏层在各时间步的隐藏状态。
+        # outputs形状是(词数, 批量大小, 2 * 隐藏单元个数)
+        outputs, _ = self.encoder(embeddings)  # output, (h, c)
+        # 连结初始时间步和最终时间步的隐藏状态作为全连接层输入。它的形状为
+        # (批量大小, 4 * 隐藏单元个数)。
+        encoding = torch.cat((outputs[0], outputs[-1]), -1)
+        outs = self.decoder(encoding)
+        return outs
+
+
+def load_pretrained_embedding(words, pretrained_vocab):
+    """从预训练好的vocab中提取出words对应的词向量"""
+    embed = torch.zeros(len(words), pretrained_vocab.vectors[0].shape[0]) # 初始化为0
+    oov_count = 0 # out of vocabulary
+    for i, word in enumerate(words):
+        try:
+            idx = pretrained_vocab.stoi[word]
+            embed[i, :] = pretrained_vocab.vectors[idx]
+        except KeyError:
+            oov_count += 0
+    if oov_count > 0:
+        print("There are %d oov words.")
+    return embed
+
+def predict_sentiment(net, vocab, sentence):
+    """sentence是词语的列表"""
+    device = list(net.parameters())[0].device
+    sentence = torch.tensor([vocab.stoi[word] for word in sentence], device=device)
+    label = torch.argmax(net(sentence.view((1, -1))), dim=1)
+    return 'positive' if label.item() == 1 else 'negative'
+
+def train(train_iter, test_iter, net, loss, optimizer, device, num_epochs):
+    net = net.to(device)
+    print("training on ", device)
+    batch_count = 0
+    for epoch in range(num_epochs):
+        train_l_sum, train_acc_sum, n, start = 0.0, 0.0, 0, time.time()
+        for X, y in train_iter:
+            X = X.to(device)
+            y = y.to(device)
+            y_hat = net(X)
+            l = loss(y_hat, y)
+            optimizer.zero_grad()
+            l.backward()
+            optimizer.step()
+            train_l_sum += l.cpu().item()
+            train_acc_sum += (y_hat.argmax(dim=1) == y).sum().cpu().item()
+            n += y.shape[0]
+            batch_count += 1
+        test_acc = evaluate_accuracy(test_iter, net)
+        print('epoch %d, loss %.4f, train acc %.3f, test acc %.3f, time %.1f sec'
+              % (epoch + 1, train_l_sum / batch_count, train_acc_sum / n, test_acc, time.time() - start))
+
+def evaluate_accuracy(data_iter, net, device=None):
+    if device is None and isinstance(net, torch.nn.Module):
+        # 如果没指定device就使用net的device
+        device = list(net.parameters())[0].device
+    acc_sum, n = 0.0, 0
+    with torch.no_grad():
+        for X, y in data_iter:
+            if isinstance(net, torch.nn.Module):
+                net.eval() # 评估模式, 这会关闭dropout
+                acc_sum += (net(X.to(device)).argmax(dim=1) == y.to(device)).float().sum().cpu().item()
+                net.train() # 改回训练模式
+            else: # 自定义的模型, 3.13节之后不会用到, 不考虑GPU
+                if('is_training' in net.__code__.co_varnames): # 如果有is_training这个参数
+                    # 将is_training设置成False
+                    acc_sum += (net(X, is_training=False).argmax(dim=1) == y).float().sum().item()
+                else:
+                    acc_sum += (net(X).argmax(dim=1) == y).float().sum().item()
+            n += y.shape[0]
+    return acc_sum / n
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+train_data, test_data = read_imdb('train'), read_imdb('test')
+vocab = get_vocab_imdb(train_data)
+
+
+batch_size = 64
+train_set = Data.TensorDataset(*preprocess_imdb(train_data, vocab))
+test_set = Data.TensorDataset(*preprocess_imdb(test_data, vocab))
+train_iter = Data.DataLoader(train_set, batch_size, shuffle=True)
+test_iter = Data.DataLoader(test_set, batch_size)
+
+for X, y in train_iter:
+    print('X', X.shape, 'y', y.shape)
+    break
+
+embed_size, num_hiddens, num_layers = 100, 100, 2
+net = BiRNN(vocab, embed_size, num_hiddens, num_layers)
+
+glove_vocab = Vocab.GloVe(name='6B', dim=100)
+
+net.embedding.weight.data.copy_(load_pretrained_embedding(vocab.itos, glove_vocab))
+net.embedding.weight.requires_grad = False # 直接加载预训练好的, 所以不需要更新它
+
+
+lr, num_epochs = 0.01, 5
+optimizer = torch.optim.Adam(filter(lambda p: p.requires_grad, net.parameters()), lr=lr)
+loss = nn.CrossEntropyLoss()
+train(train_iter, test_iter, net, loss, optimizer, device, num_epochs)
+
+print(['this', 'movie', 'is', 'so', 'great'], ':', predict_sentiment(net, vocab, ['this', 'movie', 'is', 'so', 'great']))
+print(['this', 'movie', 'is', 'so', 'great'], ':', predict_sentiment(net, vocab, ['this', 'movie', 'is', 'so', 'bad']))
